93028b20a6efa36e7a45adb4b6c997e4316b1c11
Kayeon <kky1373@naver.com>
2023-02-22 02:34:21 +0900

    모듈추가 및 로그아웃 추가

--- login/logIn.js
+++ login/logIn.js
@@ -1,67 +1,48 @@
-import { caution } from './module.mjs';
+import { caution } from "../module/func.mjs";
 
 const emailInput = document.querySelector("#email"),
-    pswordInput = document.querySelector("#password"),
-    loginBtn = document.querySelector(".login-button"),
-    errorMessage = document.querySelector(".error-message"),
-    form = document.querySelector(".login-form");
-
-// function errorEmail(){
-//     errorMessage.innerText="이메일을 입력해 주세요"
-// }
-
-// function errorPassword(){
-//     errorMessage.innerText="비밀번호를 입력해 주세요"
-// }
+  pswordInput = document.querySelector("#password"),
+  loginBtn = document.querySelector(".login-button"),
+  errorMessage = document.querySelector(".error-message"),
+  form = document.querySelector(".login-form");
 
 function login() {
-
-    if (emailInput.value == "") {
-        caution.essentialInfo(errorMessage)
-    } else if (pswordInput.value == "") {
-        caution.essentialInfo(errorMessage)
-    } else {
-        errorMessage.innerText="";
-        console.log(emailInput.value, pswordInput.value);
-        localStorage.setItem('userId',emailInput.value);
-        axios.post("http://localhost:8080/api/v1/user/login", 
-        {username: emailInput.value, password: pswordInput.value}
-        )
-        .then((res)=>{
-            console.log(res);
-                
-                localStorage.setItem('token',res.data.jwt);
-                
-                localStorage.setItem("isLoggedIn", true);
-            
-        
-       
-          location.href=`http://localhost:3000/`;
-          
-        })
-        .catch(function (error) {
-            alert(error);
-        });
-        
-    }
+  if (emailInput.value == "") {
+    caution.essentialInfo(errorMessage);
+  } else if (pswordInput.value == "") {
+    caution.essentialInfo(errorMessage);
+  } else {
+    caution.cautionMessage(errorMessage,"");
+    console.log(emailInput.value, pswordInput.value);
+    axios
+      .post("http://localhost:8080/api/v1/user/login", {
+        username: emailInput.value,
+        password: pswordInput.value,
+      })
+      .then((res) => {
+        console.log(res);
+        localStorage.setItem("token", res.data.jwt);
+        localStorage.setItem("userId", emailInput.value);
+        location.href = `http://localhost:3000/`;
+      })
+      .catch(function (error) {
+        alert(error);
+      });
+  }
 }
 
-
-
 function handleFormEnter(event) {
-    event.preventDefault();
-    if (event.keyCode === 13) {
-        login();
-    }
+  event.preventDefault();
+  if (event.keyCode === 13) {
+    login();
+  }
 }
 
 function handleButtonClick() {
-    login();
+  login();
 }
-
 
 loginBtn.addEventListener("click", handleButtonClick);
 form.addEventListener("submit", handleFormEnter);
-
 
 
--- signUp/signUp.js
+++ signUp/signUp.js
@@ -1,102 +1,123 @@
 "use strict";
 
+import { caution } from "../module/func.mjs";
 
-const email = document.querySelector("#email1"), 
-    psword = document.querySelector("#passwordbox"),
-    rePsword =document.querySelector("#repasswordbox"),
-    userName = document.querySelector("#namebox"),
-    birthYear = document.querySelector("#years"),
-    birthMonth = document.querySelector("#month"),
-    birthDay = document.querySelector("#days"),
-    phoneNumber = document.querySelector("#PNInput"),
-    getVerificationBtn = document.querySelector("#PNButton"),
-    verification = document.querySelector("#getVerification"),
-    signInBtn = document.querySelector("#signInButton"),
-    emailWarn = document.querySelector("#emailwarn"),
-    pswordWarn = document.querySelector("#pswordwarn"),
-    repswordWarn = document.querySelector("#repswordwarn"),
-    nameWarn = document.querySelector("#namewarn"),
-    discord = document.querySelector("#discord"),
-    certiWarn = document.querySelector("#certiwarn");
+const email = document.querySelector("#email1"),
+  psword = document.querySelector("#passwordbox"),
+  rePsword = document.querySelector("#repasswordbox"),
+  userName = document.querySelector("#namebox"),
+  birthYear = document.querySelector("#years"),
+  birthMonth = document.querySelector("#month"),
+  birthDay = document.querySelector("#days"),
+  phoneNumber = document.querySelector("#PNInput"),
+  signInBtn = document.querySelector("#signInButton"),
+  emailWarn = document.querySelector("#emailwarn"),
+  pswordWarn = document.querySelector("#pswordwarn"),
+  repswordWarn = document.querySelector("#repswordwarn"),
+  nameWarn = document.querySelector("#namewarn"),
+  birthWarn = document.querySelector("#birthwarn");
 
-const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+let pswordTrue = false,
+  emailTrue = false,
+  nameTrue = false,
+  birthTrue = false;
 
-const check="";
+const emailRegex = /^\S+@\S+\.\S+$/;
+const passwordRegex =
+  /^(?=.*[a-zA-Z])(?=.*[0-9])(?=.*[!@#$%^&*()_+\-=[\]{};':"\\|,.<>/?]).{8,20}$/;
+// 영문자, 숫자, 특수문자를 최소 1개 이상 포함, 길이는 8~20자로 제한
 
-function war1(){
-    emailWarn.innerText="필수 정보입니다."
-        
-    };
-function war2(){
-    pswordWarn.innerText="필수 정보입니다."
-        
-    };
-function war3(){
-    repswordWarn.innerText="필수 정보입니다."
-        
-    };
-function discode(){
-    discord.innerText="비밀번호가 일치하지 않습니다."
-            
-    };
-function war4(){
-        nameWarn.innerText="필수 정보입니다."
-        
-    };
-function war5(){
-        certiWarn.innerText="필수 정보입니다."
-        
-    };
-
-function signIn(event){
-    event.preventDefault();
-    if (emailRegex.test(email.value)) {
-       war1();
-    } else if (psword.value == check) {
-        emailWarn.innerText="";
-        war2();
-    } else if(rePsword.value == check){
-        war3();
-        pswordWarn.innerText=""
-    } else if(rePsword.value !== psword.value ){
-        discode();
-        repswordWarn.innerText="";
-    }else if(userName.value == check){
-        discord.innerText="";
-        war4();
-    }else if(verification.value == check){
-        nameWarn.innerText="";
-        war5();
-    }else{
-        certiWarn.innerText="";
-        const signInUserInfo = {
-            email : email.value,
-            password : psword.value,
-            
-            name:userName.value,
-            
-            birth:`${birthYear.value}-${birthMonth.value}-${birthDay.value}`,
-            phoneNum:phoneNumber.value,
-            gender : "MAN",
-    
-        };
-        
-        axios.post("http://localhost:8080/api/v1/user/signup", signInUserInfo,
-        {
-            headers:{
-                'Content-Type':'application/json'
-            },
-        })
-        .then((res)=>{
-            console.log(res);
-            location.href="http://localhost:3000/login/";
-        })
-        .catch(err => {
-            alert(err);
-        });
-        
+function isValidEmail() {
+  if (email.value == "") {
+    caution.essentialInfo(emailWarn);
+  } else {
+    if (!emailRegex.test(email.value)) {
+      caution.cautionMessage(emailWarn, "유효한 이메일을 입력해 주세요.");
+    } else {
+      caution.cautionMessage(emailWarn, "");
+      emailTrue = true;
     }
-
+  }
+}
+function passwordCorrect() {
+  if (psword.value != rePsword.value) {
+    caution.cautionMessage(repswordWarn, "비밀번호가 일치하지 않습니다.");
+  } else {
+    caution.cautionMessage(repswordWarn, "");
+    pswordTrue = true;
+  }
 }
 
-    signInBtn.addEventListener("click",signIn);
+function isValidPassword() {
+  if (psword.value == "") {
+    caution.essentialInfo(pswordWarn);
+  } else {
+    if (!passwordRegex.test(psword.value)) {
+      caution.cautionMessage(
+        pswordWarn,
+        "비밀번호는 영문자+숫자+특수문자 조합으로 8~20자리 사용해야 합니다."
+      );
+    } else {
+      caution.cautionMessage(pswordWarn, "");
+      passwordCorrect();
+    }
+  }
+}
+
+function requiredName() {
+  if (userName.value == "") {
+    caution.essentialInfo(nameWarn);
+  } else {
+    caution.cautionMessage(nameWarn, "");
+    nameTrue = true;
+  }
+}
+function requiredBirth() {
+  if (birthYear.value == "" || birthDay.value == "") {
+    caution.essentialInfo(birthWarn);
+  } else {
+    caution.cautionMessage(birthWarn, "");
+    birthTrue = true;
+  }
+}
+
+function collectInformation() {
+  const data = {
+    email: email.value,
+    password: psword.value,
+    name: userName.value,
+    birth: `${birthYear.value}-${birthMonth.value}-${birthDay.value}`,
+    phoneNum: phoneNumber.value,
+    gender: "MAN",
+  };
+  return data;
+}
+
+// 로그인함수
+function signIn(event) {
+  event.preventDefault();
+  if (!(nameTrue && pswordTrue && emailTrue && birthTrue)) {
+    event.preventDefault();
+    isValidEmail();
+    isValidPassword();
+    requiredName();
+    requiredBirth();
+  } else {
+    const UserInfo = collectInformation();
+    axios
+      .post("http://localhost:8080/api/v1/user/signup", UserInfo, {
+        headers: {
+          "Content-Type": "application/json",
+        },
+      })
+      .then((res) => {
+        console.log(res);
+        location.href = "http://localhost:3000/login/";
+      })
+      .catch((err) => {
+        alert(err);
+      });
+  }
+}
+
+signInBtn.addEventListener("click", signIn);
